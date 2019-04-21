2b8ab9e30a397750eb5a9dd00db0e30cf05ad728
MohamedHassan275 <mohamed209059@gmail.com>
2019-04-22 01:25:00 +0200

    Initial commit

--- app/src/main/java/com/mohmedhassan/otleb/RegisterActivity.java
+++ app/src/main/java/com/mohmedhassan/otleb/RegisterActivity.java
@@ -1,16 +1,27 @@
 package com.mohmedhassan.otleb;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Context context;
     Button Btn_Register;
     TextView Tv_Regiseter_Login;
+    EditText Ed_birthDay_register;
+    private int mYear, mMonth, mDay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +31,33 @@ public class RegisterActivity extends AppCompatActivity {
 
         Btn_Register = findViewById(R.id.btn_register_register);
         Tv_Regiseter_Login = findViewById(R.id.tv_register_login);
+        Ed_birthDay_register = findViewById(R.id.Ed_birthDay_register);
+
+        Ed_birthDay_register.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(RegisterActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                Ed_birthDay_register.setText(dayOfMonth + "/" + (monthOfYear+1) + "/" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+            });
 
         Tv_Regiseter_Login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,5 +75,8 @@ public class RegisterActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
+
 }
